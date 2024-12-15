05e944b7e16d9b7edf67839545a20e1a1514de82
Pinta365 <hello@pinta.land>
2024-12-15 01:46:00 +0100

    support  different compilers (rust and as)

--- README.md
+++ README.md
@@ -1,11 +1,53 @@
 # WASM-DENO-BOILERPLATE (WIP)
 
-Start of a WebAssembly + Deno boilerplate project. Builds WebAssembly from Rust code.
+This repository provides a boilerplate for creating WebAssembly (WASM) modules with Deno using either Rust or AssemblyScript. It includes the
+necessary files and configuration to get started quickly.
 
-```bash
-deno task build
-deno task test
-```
+## Getting Started
 
-1. Format code -> Build Rust WASM -> Clean build artifacts Will output the WASM and javascript bindings in /lib
-2. Run tests
+### Prerequisites
+
+- **Deno:** Requires at least v2.1.0 for WASM module support. [Install Deno](https://deno.land/manual/getting_started/installation).
+
+### Compilers
+
+- **AssemblyScript:** No additional requirements, as the compiler is included as an npm package.
+
+- **Rust:**
+  - Requires Rust and Cargo. [Install Rust](https://www.rust-lang.org/tools/install)
+  - Ensure your Rust installation is up-to-date. You can check your Rust version with `rustc --version` and update it with `rustup update`.
+
+### Usage
+
+1. Generate a new project using the boilerplate generator:
+
+   ```bash
+   deno run -RWN jsr:@pinta365/wasm-deno-boilerplate --name <project_name> [options]
+   ```
+
+   Replace `<project_name>` with the desired name for your project. Default compiler language is set to AssemblyScript.
+
+   Available options:
+   ```
+   --name <project_name>         Name of the project directory to create (required)
+   --lang <rust|assemblyscript>  Specify the language for WASM compilation (assemblyscript, rust) (default: assemblyscript)
+   --verbose, -v                 Enable verbose output
+   --help, -h                    Show this help message
+   ```
+
+2. Navigate to the project directory:
+
+   ```
+   cd <project_name>
+   ```
+
+3. Build the WASM test module:
+
+   ```bash
+   deno task build:demo
+   ```
+
+   The `build:demo` task will go through these steps.
+   - Format code
+   - Compile the source code into a WASM binary (`/wasm`).
+   - Run tests
---
+++ mod.ts
@@ -0,0 +1,109 @@
+import { ensureDir } from "jsr:@std/fs";
+import * as path from "jsr:@std/path";
+import { parseArgs } from "jsr:@std/cli/parse-args";
+import { bold, cyan, green, red, yellow } from "jsr:@std/fmt/colors";
+
+const parsedArgs = parseArgs(Deno.args, {
+    string: ["name", "lang"],
+    boolean: ["verbose", "help"],
+    alias: {
+        verbose: ["v"],
+        help: ["h"],
+    },
+    default: {
+        "verbose": false,
+        "help": false,
+        "lang": "assemblyscript",
+    },
+});
+
+if (parsedArgs.help) {
+    console.log(
+        `${green("Usage:")} deno run -RWN jsr:@pinta365/wasm-deno-boilerplate ${green("--name <project_name>")} [options]
+  
+  ${yellow("Options:")}
+    ${green("--name <project_name>")}         ${bold("Name of the project directory to create (required)")}
+    ${green("--lang <rust|assemblyscript>")}  ${
+            bold("Specify the language to use for WASM compilation (assemblyscript, rust) (default: assemblyscript)")
+        }
+    ${green("--verbose")}, ${green("-v")}                 ${bold("Enable verbose output")}    
+    ${green("--help")}, ${green("-h")}                    ${bold("Show this help message")}`,
+    );
+    Deno.exit(0);
+}
+
+const projectName = parsedArgs.name;
+let projectDir: string;
+const baseUrl = "https://raw.githubusercontent.com/Pinta365/wasm-deno-boilerplate/main";
+
+if (!projectName) {
+    console.error(`${red("Error:")} Please provide a project name using ${green("--name <project_name>")}`);
+    Deno.exit(1);
+}
+
+async function downloadAndSave(filePath: string) {
+    const langFolder = parsedArgs.lang === "rust" ? "rust" : "assemblyscript";
+
+    if (parsedArgs.verbose) {
+        console.log(cyan(`Downloading ${filePath}...`));
+    }
+    const response = await fetch(`${baseUrl}/${langFolder}/${filePath}`);
+    if (!response.ok) {
+        throw new Error(`Failed to download ${filePath}`);
+    }
+    const content = await response.text();
+    await Deno.writeTextFile(path.join(projectDir, filePath), content);
+}
+
+try {
+    projectDir = path.join(Deno.cwd(), projectName);
+    await ensureDir(projectDir);
+
+    if (parsedArgs.lang === "assemblyscript") {
+        await downloadAndSave("README.md");
+        await downloadAndSave("main.ts");
+        await downloadAndSave("main_test.ts");
+        await downloadAndSave("LICENSE");
+        await downloadAndSave("deno.json");
+        await downloadAndSave(".gitignore");
+
+        await ensureDir(path.join(projectDir, "lib/"));
+        await downloadAndSave("lib/ascompile.ts");
+        await downloadAndSave("lib/customTransform.ts");
+
+        await ensureDir(path.join(projectDir, "as_lib/src"));
+        await downloadAndSave("as_lib/src/index.ts");
+    } else if (parsedArgs.lang === "rust") {
+        await downloadAndSave("deno.json");
+        await downloadAndSave("main.ts");
+        await downloadAndSave("main_test.ts");
+        await downloadAndSave("README.md");
+        await downloadAndSave("LICENSE");
+        await downloadAndSave(".gitignore");
+        await downloadAndSave(".rustfmt.toml");
+        await downloadAndSave("Cargo.toml");
+
+        await ensureDir(path.join(projectDir, "rs_lib/src"));
+
+        await downloadAndSave("rs_lib/Cargo.toml");
+        await downloadAndSave("rs_lib/src/lib.rs");
+    } else {
+        console.error(`${red("Error:")} ${yellow(parsedArgs.lang)} is not a supported language for compile.`);
+        Deno.exit(1);
+    }
+
+    console.log(bold(green(`Project "${projectName}" created successfully!`)));
+    console.log(yellow(`Navigate to the project directory: ${bold(`cd ${projectName}`)}`));
+    console.log(yellow(`Run the build task: ${bold("deno task build:demo")}`));
+} catch (error: unknown) {
+    if (error instanceof Error) {
+        console.error(`${red("Error:")} ${error.message}`);
+        if (parsedArgs.verbose) {
+            console.error("Error details:", error);
+            console.error("Parsed arguments:", parsedArgs);
+        }
+    } else {
+        console.error(`${red("Error:")} An unexpected error occurred.`);
+    }
+    Deno.exit(1);
+}
